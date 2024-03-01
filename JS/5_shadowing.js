5df3735b091a787ea603e3ae610adcc2a534f661
Abhishek <abhishekgoel@gofynd.com>
2024-03-01 23:58:46 +0530

    shadowing illegal shadowing

---
+++ JS/5_shadowing.js
@@ -0,0 +1,25 @@
+// //shadowing 
+// var a=10;
+// {
+//     var a=100;
+//     console.log(a); //100
+// }
+// console.log(a);//100
+
+// //shadowing 
+// two variables same name but different scope 
+// var a=10;
+// {
+//     let a=100;
+//     console.log(a); //100
+// }
+// console.log(a);//10
+
+//illegal shadowing 
+// two variables same name and same scope 
+// let a=10;
+// {
+//     var a=100;
+//     console.log(a);
+// }
+// console.log(a);
